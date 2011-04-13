a5e7587bec3f12b1bdb80b5856c625a4ff4cc624
Ben Stephens <zeffrin@gmail.com>
2011-04-14 06:35:59 +1000

    Create entity and inherit it into living.

--- ZesmiCore/living.hpp
+++ ZesmiCore/living.hpp
@@ -1,9 +1,9 @@
 #ifndef LIVING_HPP_INCLUDED
 #define LIVING_HPP_INCLUDED
 
-#include "vector.hpp"
+#include "entity.hpp"
 
-class Living
+class Living : public Entity
 {
     public:
         Living();
@@ -12,13 +12,9 @@ class Living
         short int getHealth();
         short int getArmor();
         void takeDamage(int damage);
-        Vector getVector();
-        Position getPosition();
-
 
     private:
         short int _health;
         short int _armor;
-        Vector _vector;
 };
 #endif // LIVING_HPP_INCLUDED
