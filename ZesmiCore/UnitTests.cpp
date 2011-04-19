4f6056bd62942fd6bcaca6f95f227fefc79e7859
Ben Stephens <zeffrin@gmail.com>
2011-04-19 16:16:31 +1000

    Added more packet tests

--- ZesmiCore/UnitTests.cpp
+++ ZesmiCore/UnitTests.cpp
@@ -431,31 +431,39 @@ bool testAllHandler(Packet *p, Connection *sender)
         return false;
     }
 
+    printf("receiving %d\n", p->PacketID);
+    bool error = false;
     switch(p->PacketID)
     {
-        case P_KEEPALIVE: { return true; }
+        case P_KEEPALIVE: break;
         case P_LOGIN:
         {
             LoginPacket *t = (LoginPacket*)p;
-            if(t->PacketID == P_LOGIN &&
+            if(!(t->PacketID == P_LOGIN &&
                t->ProtocolVersion == 15 &&
                strcmp(t->Username, "Zeffrin") == 0 &&
                strcmp(t->VerificationKey, "-") == 0 &&
                t->MapSeed == 101 &&
-               t->Dimension == 5)
-            {
-               return true;
-            }
+               t->Dimension == 5)) { error = true; }
             break;
         }
         case P_HANDSHAKE:
-        {
-            if(strcmp(((HandShakePacket*)p)->Username, "Zeffrin") == 0)
-                return true;
+            if(strcmp(((HandShakePacket*)p)->Username, "Zeffrin") != 0) error = true;
             break;
-        }
+        case P_CHAT:
+            {
+                char *bla = ((ChatPacket*)p)->Message;
+                if(strcmp(((ChatPacket*)p)->Message, "Test message a-zA-Z0-9") != 0) error = true;
+            }
+            break;
+        case P_UPDATETIME:
+            if(((UpdateTimePacket*)p)->Time != 104) error = true;
+            break;
+        default:
+            error = true;
+            break;
     }
-    return false;
+    return !error;
 }
 
 int testAllPackets()
@@ -478,24 +486,245 @@ int testAllPackets()
     for(unsigned int i = 0; i < PACKETCOUNT ; i++)
     {
         bool unknown = false;
-
         switch(i)
         {
             case P_KEEPALIVE:
+            {
                 KeepAlivePacket p;
                 p.PacketID = P_KEEPALIVE;
                 c->SendPacket((Packet*)&p);
                 break;
+            }
             case P_LOGIN:
-                LoginPacket p2;
-                p2.PacketID = P_HANDSHAKE;
-                p2.ProtocolVersion = 15;
-                strcpy(p2.Username, "Zeffrin");
-                strcpy(p2.VerificationKey, "-");
-                p2.MapSeed = 101;
-                p2.Dimension = 5;
-                c->SendPacket((Packet*)&p);
+            {
+                LoginPacket p1;
+                p1.PacketID = P_LOGIN;
+                p1.ProtocolVersion = 15;
+                strcpy(p1.Username, "Zeffrin");
+                strcpy(p1.VerificationKey, "-");
+                p1.MapSeed = 101;
+                p1.Dimension = 5;
+                c->SendPacket((Packet*)&p1);
                 break;
+            }
+            case P_HANDSHAKE:
+            {
+                HandShakePacket p2;
+                p2.PacketID = P_HANDSHAKE;
+                strcpy(p2.Username, "Zeffrin");
+                c->SendPacket((Packet*)&p2);
+                break;
+            }
+            case P_CHAT:
+            {
+                ChatPacket p3;
+                p3.PacketID = P_CHAT;
+                strcpy(p3.Message, "Test message a-zA-Z0-9");
+                c->SendPacket((Packet*)&p3);
+                break;
+            }
+            case P_UPDATETIME:
+            {
+                UpdateTimePacket p4;
+                p4.PacketID = P_UPDATETIME;
+                p4.Time = 104;
+                c->SendPacket((Packet*)&p4);
+                break;
+            }
+            case P_PLAYERINVENTORY:
+            {
+                PlayerInventoryPacket p5;
+                p5.PacketID = P_PLAYERINVENTORY;
+                p5.EntityID = 101;
+                p5.Slot = 5;
+                p5.ItemID = 101;
+                p5.ItemDamage = 10;
+                c->SendPacket((Packet*)&p5);
+                break;
+            }
+            case P_SPAWNPOSITION:
+            {
+                SpawnPositionPacket p6;
+                p6.PacketID = P_SPAWNPOSITION;
+                p6.X = 101;
+                p6.Y = 202;
+                p6.Z = 303;
+                c->SendPacket((Packet*)&p6);
+                break;
+            }
+            case P_ACTION:
+            {
+                ActionPacket p7;
+                p7.PacketID = P_ACTION;
+                p7.PlayerEntity = 101;
+                p7.TargetEntity = 202;
+                p7.IsLeftClick = 1;
+                c->SendPacket((Packet*)&p7);
+                break;
+            }
+            case P_HEALTH:
+            {
+                HealthPacket p8;
+                p8.PacketID = P_HEALTH;
+                p8.Health = 88;
+                c->SendPacket((Packet*)&p8);
+                break;
+            }
+            case P_RESPAWN:
+            {
+                RespawnPacket p9;
+                p9.PacketID = P_RESPAWN;
+                c->SendPacket((Packet*)&p9);
+                break;
+            }
+            case P_FLYING:
+            {
+                FlyingPacket p10;
+                p10.PacketID = P_FLYING;
+                p10.Flying = 1;
+                c->SendPacket((Packet*)&p10);
+                break;
+            }
+            case P_PLAYERPOSITION:
+            {
+                PlayerPositionPacket p11;
+                p11.PacketID = P_PLAYERPOSITION;
+                p11.X = 101.1111f;
+                p11.Y = 202.2222f;
+                p11.Stance = 111.1111f;
+                p11.Z = 303.3333f;
+                p11.Flying = 1;
+                c->SendPacket((Packet*)&p11);
+                break;
+            }
+            case P_PLAYERLOOK:
+            {
+                PlayerLookPacket p12;
+                p12.PacketID = P_PLAYERLOOK;
+                p12.Yaw = 101.1111f;
+                p12.Pitch = 202.2222f;
+                p12.Flying = 1;
+                c->SendPacket((Packet*)&p12);
+                break;
+            }
+            case P_PLAYERLOOKMOVE:
+            {
+                PlayerLookMovePacket p13;
+                p13.PacketID = P_PLAYERLOOKMOVE;
+                p13.X = 101.11111f;
+                p13.Y = 202.22222f;
+                p13.Stance = 303.33333f;
+                p13.Z = 404.44444f;
+                p13.Yaw = 101.11111f;
+                p13.Pitch = 202.2222f;
+                c->SendPacket((Packet*)&p13);
+                break;
+            }
+            case P_BLOCKDIG:
+            {
+                BlockDigPacket p14;
+                p14.PacketID = P_BLOCKDIG;
+                p14.Status = 1;
+                p14.X = 101;
+                p14.Y = 202;
+                p14.Z = 303;
+                p14.Face = 6;
+                c->SendPacket((Packet*)&p14);
+                break;
+            }
+            case P_PLACE:
+            {
+                PlacePacket p15;
+                p15.PacketID = P_PLACE;
+                p15.X = 101;
+                p15.Y = 202;
+                p15.Z = 303;
+                p15.Direction = 101;
+                p15.ItemID = 32;
+                p15.StackSize = 64;
+                p15.ItemDamage = 10;
+                c->SendPacket((Packet*)&p15);
+                break;
+            }
+            case P_BLOCKITEMSWITCH:
+            {
+                BlockItemSwitchPacket p16;
+                p16.PacketID = P_BLOCKITEMSWITCH;
+                p16.ID = 101;
+                c->SendPacket((Packet*)&p16);
+                break;
+            }
+            case P_SLEEP:
+            {
+                SleepPacket p17;
+                p17.PacketID = P_SLEEP;
+                p17.EntityID = 101;
+                p17.field_22042_e = 202;
+                p17.field_22040_b = 303;
+                p17.field_22044_c = 101;
+                p17.field_22043_d = 202;
+                c->SendPacket((Packet*)&p17);
+                break;
+            }
+            case P_ARMANIMATION:
+            {
+                ArmAnimationPacket p18;
+                p18.PacketID = P_ARMANIMATION;
+                p18.EntityID = 101;
+                p18.Animate = 1;
+                c->SendPacket((Packet*)&p18);
+                break;
+            }
+            case P_PACKET19:
+            {
+                P19Packet p19;
+                p19.PacketID = P_PACKET19;
+                p19.EntityID = 101;
+                p19.State = 1;
+                c->SendPacket((Packet*)&p19);
+                break;
+            }
+            case P_NAMEDENTITYSPAWN:
+            {
+                NamedEntitySpawnPacket p20;
+                p20.PacketID = P_NAMEDENTITYSPAWN;
+                p20.EntityID = 101;
+                strcpy(p20.Name, "Zeffrin");
+                p20.X = 101;
+                p20.Y = 202;
+                p20.Z = 303;
+                p20.Rotation = 128;
+                p20.Pitch = 156;
+                p20.CurrentItem = 22;
+                c->SendPacket((Packet*)&p20);
+                break;
+            }
+            case P_PICKUPSPAWN:
+            {
+                PickupSpawnPacket p21;
+                p21.PacketID = P_PICKUPSPAWN;
+                p21.EntityID = 101;
+                p21.ItemID = 22;
+                p21.Count = 2;
+                p21.ItemDamage = 5;
+                p21.X = 101;
+                p21.Y = 202;
+                p21.Z = 303;
+                p21.Rotation = 255;
+                p21.Pitch = 100;
+                p21.Roll = 30;
+                c->SendPacket((Packet*)&p21);
+                break;
+            }
+            case P_COLLECT:
+            {
+                CollectPacket p22;
+                p22.PacketID = P_COLLECT;
+                p22.CollectedEntityID = 101;
+                p22.CollectorEntityID = 202;
+                c->SendPacket((Packet*)&p22);
+                break;
+            }
             default:
                 unknown = true;
                 break;
